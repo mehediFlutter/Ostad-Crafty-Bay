b123b82a454c98c78b69d2486d0f297d04bba7a6
mehediFluttere <mehedii.edu.bd@gmail.com>
2025-01-23 19:11:43 +0600

    Reusable mother widget added

---
+++ lib/application/presentation/ui/re_usable_mother_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ReUsableMotherWidget extends StatelessWidget {
+  final Widget child;
+  const ReUsableMotherWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: child),
+    );
+  }
+}
--- lib/application/presentation/ui/screens/home.dart
+++ lib/application/presentation/ui/screens/home.dart
@@ -1,9 +1,5 @@
-import 'package:crafty_bay/application/presentation/const.dart';
+import 'package:crafty_bay/application/presentation/ui/re_usable_mother_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import '../widgets/custom_app_bar.dart';
-import 'utility/image_assets/image_assets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,27 +11,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(),
-      body: SizedBox(
-        width: double.infinity,
+    return ReUsableMotherWidget(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 4,
-              child: SvgPicture.asset(ImageAssets.craftyBayLogoSVG)),
-           Spacer(),
-            CircularProgressIndicator(),
-            height16,
-            Text("Version:1.0.0"),
-            height16,
-          ],
-        ),
-      ),
-    );
+      children: [],
+    ));
   }
 }
-
-
