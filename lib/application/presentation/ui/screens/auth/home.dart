77bdd240109ff7d69ceceaf5b44b5cdfb5960f28
mehediFluttere <mehedii.edu.bd@gmail.com>
2025-01-24 23:41:24 +0600

    Home Slier

--- lib/application/presentation/ui/screens/auth/home.dart
+++ lib/application/presentation/ui/screens/auth/home.dart
@@ -1,9 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:crafty_bay/application/presentation/const.dart';
-import 'package:crafty_bay/application/presentation/ui/re_usable_mother_widget.dart';
+import 'package:crafty_bay/application/presentation/ui/screens/utility/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../widgets/circular_icon_button.dart';
+import '../../widgets/home_slider.dart';
 import '../utility/image_assets/image_assets.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -34,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 icon: Icons.call,
                 onTap: () {},
               ),
-               width8,
+              width8,
               CircularIconButton(
                 icon: Icons.notifications_none,
                 onTap: () {},
@@ -42,8 +44,36 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                    prefixIcon: Icon(Icons.search, color: Colors.grey),
+                    hintText: 'Search',
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: Colors.grey),
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                    focusedBorder:
+                        OutlineInputBorder(borderSide: BorderSide.none),
+                    enabledBorder:
+                        OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                ),
+                HomeSlider()
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
 
